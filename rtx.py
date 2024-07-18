ac35eaa3ce650b75448085f5eacbb2b309a701a9
«Bilalline» <bilalline@gmail.com>
2024-07-18 20:08:44 +0300

    добовление впиветствия бота и собщение о выполнении задачи

--- rtx.py
+++ rtx.py
@@ -3,6 +3,10 @@ import asyncio
 import rtx_api_3_5 as rtx_api
 from telebot.async_telebot import AsyncTeleBot
 from dotenv import load_dotenv
+import random
+
+options = ["Работаю", "Выполняю", "Обождите", "Занимаюсь", "В работе", "Делаю чудо", "Бип буп бип буп"]
+
 
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
@@ -10,10 +14,17 @@ TOKEN = os.getenv("TOKEN")
 # Создаем объект бота
 bot = AsyncTeleBot(TOKEN)
 
+@bot.message_handler(commands=['help', 'start'])
+async def send_welcome(message):
+    await bot.reply_to(message, """\
+Приветствую, Я чатбот на осове ChatRtx.
+Сейчас я работаю на чистом энтузиазме и жгу электроэнергию в квартире. для получения этого сообщения выполни команду /help.\
+""")
 
 @bot.message_handler(func=lambda message: True)
 async def handle_message(message):
-    await bot.reply_to(message, 'wait')
+    selected_option = random.choice(options)
+    await bot.reply_to(message, selected_option)
     res = rtx_api.send_message(message.text)
     # print(response)
     await bot.reply_to(message, res)

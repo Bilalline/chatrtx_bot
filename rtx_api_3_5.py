8ceeb97be63cc432e7f39c78b6c4bee11556dd1c
Bilalline <148867855+Bilalline@users.noreply.github.com>
2024-07-21 17:55:28 +0300

    добавление parse_mode='markdown' (#1)
    Co-authored-by: «Bilalline» <«bilalline@gmail.com»>

--- rtx_api_3_5.py
+++ rtx_api_3_5.py
@@ -3,27 +3,10 @@ import string
 import json
 import requests
 
-port = 20290
+port = 43978
 
-# def find_chat_with_rtx_port():
-#     global port
-#     connections = psutil.net_connections(kind='inet')
-#     for host in connections:
-#         try:
-#             if host.pid:
-#                 process = psutil.Process(host.pid)
-#                 if "ChatWithRTX" in process.exe():
-#                     test_port = host.laddr.port
-#                     url = f"http://192.168.1.57:20290/queue/join"
-#                     response = requests.post(url, data="", timeout=0.05)
-#                     if response.status_code == 422:
-#                         port = test_port
-#                         return
-#         except:
-#             pass
 
 def join_queue(session_hash, fn_index, port, chatdata):
-    #fn_indexes are some gradio generated indexes from rag/trt/ui/user_interface.py
     python_object = {
         "data": chatdata,
         "event_data": None,
@@ -32,20 +15,17 @@ def join_queue(session_hash, fn_index, port, chatdata):
     }
     json_string = json.dumps(python_object)
 
-    url = f"http://192.168.1.57:20290/queue/join"
+    url = f"http://192.168.1.57:{port}/queue/join"
     response = requests.post(url, data=json_string)
-    # print("Join Queue Response:", response.json())
 
 def listen_for_updates(session_hash, port):
-    url = f"http://192.168.1.57:20290/queue/data?session_hash={session_hash}"
+    url = f"http://192.168.1.57:{port}/queue/data?session_hash={session_hash}"
 
     response = requests.get(url, stream=True)
     for line in response.iter_lines():
         if line:
             try:
                 data = json.loads(line[5:])
-                # if data['msg'] == 'process_generating':
-                #     print(data['output']['data'][0][0][1])
                 if data['msg'] == 'process_completed':
                     return data['output']['data'][0][0][1]
             except Exception as e:
